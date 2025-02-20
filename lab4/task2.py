bf5d20b97c1dafdd2c0601259bba3d0aa6baa774
Алиев Канат <alievkanat@MacBook-Pro-Dana.local>
2025-02-20 17:41:47 +0500

    exercises lab4

--- lab4/task2.py
+++
@@ -1,5 +0,0 @@
-import math
-height = float(input("Height: "))
-first_value = float(input("Base, first value "))
-second_value = float(input("Base, second value: "))
-print(f"Expected Output {(first_value + second_value)* height / 2 :.1f}")
